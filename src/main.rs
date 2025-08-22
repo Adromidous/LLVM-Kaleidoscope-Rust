d3ffae4188c5aea540325c98a51c44850cd2de35
Adromidous <t24kumar17@gmail.com>
2025-08-22 00:32:19 -0400

    Framework for initial lexer state, needs testing

--- src/main.rs
+++ src/main.rs
@@ -26,17 +26,52 @@ fn main() {
         let character: Option<char> = characters.next();
 
         if let Some(valid_char) = character {
-            identifier_str.push(valid_char);     
 
-            if valid_char == ' ' {
-                println!("{}", identifier_str);
+            if valid_char == ' ' { //WHITESPACES
+                continue;
+            }
+
+            else if valid_char.is_alphabetic() { //DEF, EXTERN, IDENTIFIER
+                identifier_str.push(valid_char);
+                
+                loop {
+                    let character = characters.next();
+                    match character {
+                        Some(val) => identifier_str.push(val),
+                        None => break 
+                    }
+                }
+
+                match identifier_str.as_str() {
+                    "def" => tokens.push(Token::Def),
+                    "extern" => tokens.push(Token::Extern),
+                    _ => tokens.push(Token::Identifier),
+                }
+
+                identifier_str = String::from("");
+
+            } 
+
+            else if valid_char.is_numeric() {   //NUMBER
+                identifier_str.push(valid_char);
+
+                loop {
+                    let character = characters.next();
+                    match character {
+                        Some(val) => identifier_str.push(val),
+                        None => break
+                    }
+                }
+
+                tokens.push(Token::Number);
                 identifier_str = String::from("");
             }
-        } else { //REACH EOF
+
+        } else { //EOF
+            tokens.push(Token::EOF);
             break;
         }
 
     }
 
-    tokens.push(Token::EOF);
 }
