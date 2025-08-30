5c8321576c78cd9e815e0a03a043196f78dcde19
Adromidous <t24kumar17@gmail.com>
2025-08-29 23:41:48 -0400

    Added parser return token function

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,15 @@ enum Token {
 
     // Primary
     Identifier,         //[a-zA-Z][a-zA-Z0-9]*
-    Number              //[0-9]+
+    Number,             //[0-9]+
+
+    // Miscellaneous
+    Whitespace,
+    Error
 }
 
 struct ExprAST {
     Type: String
-    //NEED TO INCLUDE FIELD THAT HOLDS THE DATA FOR THE ACTUAL AST OBJECT
 }
 
 struct VariableExprAST {
@@ -138,11 +141,108 @@ impl Lexer {
 
     }
 
-    fn generate_tokens(&self){
+    fn print_tokens(&self){
         for tok in self.tokens.iter() {
             println!("TOKEN: {:#?}", tok);
         }
     }
+
+}
+
+struct Parser {
+    identifier_str: String,
+    num_val: u32,
+    contents: String,
+}
+
+impl Parser {
+
+    fn new(filename: &String) -> Self {
+        let mut contents = fs::read_to_string(filename)
+                            .expect("FILE NAME NOT VALID");
+
+        let mut identifier_str = String::from("");
+
+        Parser {
+            identifier_str: identifier_str,
+            num_val: 0,
+            contents: contents,
+        }
+    }
+
+    fn gettoken(&mut self) -> Token {
+        self.identifier_str = String::from("");
+
+        let mut characters = self.contents.chars();
+
+        let character: Option<char> = characters.next();
+
+        if let Some(valid_char) = character {
+
+            if valid_char == ' ' { //WHITESPACES
+                return Token::Whitespace; 
+            }
+
+            else if valid_char.is_alphabetic() { //DEF, EXTERN, IDENTIFIER
+                self.identifier_str.push(valid_char);
+                
+                while let Some(valid_char) = characters.next() {
+                    
+                    if valid_char.is_alphabetic() || valid_char.is_numeric() {
+                        self.identifier_str.push(valid_char);    
+                    }
+
+                    else {
+                        break;
+                    }
+
+                }
+
+                match self.identifier_str.as_str() {
+                    "def" => return Token::Def,
+                    "extern" => return Token::Extern,
+                    _ => return Token::Identifier,
+                }
+
+
+            } 
+
+            else if valid_char.is_numeric() {   //NUMBER
+                self.identifier_str.push(valid_char);
+
+                while let Some(valid_char) = characters.next() {
+
+                    if valid_char.is_numeric() {
+                        self.identifier_str.push(valid_char);
+                    }
+
+                    else {
+                        break;
+                    }
+                }
+                
+                let store_num = self.identifier_str.parse();
+
+                match store_num {
+                    Ok(val) => self.num_val = val,
+                    _ => println!("EXPECTED NUMBER!")
+                }
+
+                return Token::Number;
+            }
+
+            else { //EOF
+                return Token::EOF;
+            }
+        } 
+
+        else {
+            return Token::Error;
+        }
+    }
+
+   fn retAST(&mut self) -> ExprAST {
+   }
 
 }
 
@@ -151,5 +251,7 @@ fn main() {
 
     let mut lex1 = Lexer::new(&file_path);
 
-    lex1.generate_tokens();
+    lex1.print_tokens();
+
+    let mut parser1 = Parser::new(&file_path);
 }
