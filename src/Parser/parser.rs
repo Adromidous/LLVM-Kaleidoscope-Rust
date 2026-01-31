403628bd92d1c4c0bbcfa6e385a02c098092f2c6
Adromidous <t24kumar17@gmail.com>
2026-01-31 13:01:17 -0500

    Adding parser functions

--- src/Parser/parser.rs
+++ src/Parser/parser.rs
@@ -1,210 +1,133 @@
-use crate::Token::token::{self as tok, BinaryExprAST, EOFExprAST, EqualExprAST, ExprAST, NumberExprAST, ParenthesisExprAST, VariableExprAST};
-use crate::Lexer::lexer as lex;
+use std::fs;
 
-use std::{str::Chars, iter::Peekable};
-use std::{fs::File, io::Write};
+use crate::Token::token::*;
+use std::str::Chars;
+use std::iter::Peekable;
 
 pub struct Parser {
-    root: Box<dyn tok::Visit>,
+    pub tree: ExprAST
 }
 
 impl Parser {
+    pub fn new(filename: &str) -> Parser {
+        let contents = fs::read_to_string(filename)
+                    .expect("FILE NAME NOT VALID");
 
-    pub fn new(lex: &mut lex::Lexer) -> Parser{
-        let mut characters = lex.contents.chars().peekable();
+        let mut tokens: Vec<Token> = Vec::new();
+        let mut characters = contents.chars().peekable();
 
         Parser {
-            root: Self::recursive_descent(&mut characters),
+            tree: ExprAST {
+                child: Self::recursive_descent(),
+            }
         }
     }
 
-    fn recursive_descent(char_iter: &mut Peekable<Chars>) -> Box<dyn tok::Visit> { 
-        let (curr_tok, curr_str) = Self::gettok(char_iter);
-
-        match curr_tok {
-
-            tok::Token::Identifier => {
-                if Self::lookahead_tok(char_iter) == tok::Token::Operator {
-                    return Box::new(BinaryExprAST {
-                        lhs: Box::new(VariableExprAST {name: curr_str}),
-                        operator: Self::gettok(char_iter).1,
-                        rhs: Self::recursive_descent(char_iter)
-                    })
-                } else if Self::lookahead_tok(char_iter) == tok::Token::Equal {
-                    Self::gettok(char_iter);
-                    return Box::new(EqualExprAST {
-                        lhs: VariableExprAST {name: curr_str},
-                        rhs: Self::recursive_descent(char_iter)
-                    })
-                } else {      
-                    return Box::new(VariableExprAST { name: curr_str});
-                }
-            },
-
-            tok::Token::Number => {
-                let value: usize = curr_str.parse().unwrap();
-
-                if Self::lookahead_tok(char_iter) == tok::Token::Operator {
-                    let op = Self::gettok(char_iter);
-
-                    // if Self::lookahead_tok(char_iter) == tok::Token::Number {
-                    //     let rhs_value: usize = Self::gettok(char_iter).1.parse().unwrap();
-                    //     match op.1.as_str() {
-                    //         "+" => {
-                    //             return Box::new(NumberExprAST {
-                    //                 value: value + rhs_value,
-                    //             })
-                    //         },
-
-                    //         "-" => {
-                    //             return Box::new(NumberExprAST {
-                    //                 value: value - rhs_value,
-                    //             })
-                    //         },
-
-                    //         "/" => {
-                    //             return Box::new(NumberExprAST {
-                    //                 value: value / rhs_value,
-                    //             })
-                    //         },
-
-                    //         "*" => {
-                    //             return Box::new(NumberExprAST {
-                    //                 value: value * rhs_value,
-                    //             })
-                    //         },
-
-                    //         _ => {
-                    //             panic!("Improper arithmetic operation")
-                    //         }
-                    //     }
-                    // }
-                    return Box::new(BinaryExprAST {
-                        lhs: Box::new(NumberExprAST { value: value}),
-                        operator: op.1,
-                        rhs: Self::recursive_descent(char_iter)
-                    })
-                } else {
-                    return Box::new(NumberExprAST { value: value});
-                }
-            },
-
-            tok::Token::EOF => {
-                return Box::new(EOFExprAST{})
-            },
-
-            _ => { // FIXME: COVER CASES FOR OPERATOR
-                return Box::new(
-                    ExprAST {
-                        child: Vec::new(), //FIXME: NEED TO FIND ALTERNATE SOLUTION FOR EOF
-                    }
-                );
-            },
-
+    fn parse_identifier(identifier_name: String) -> VariableExprAST {
+        VariableExprAST { 
+            name: (identifier_name) 
         }
     }
 
-    fn gettok(char_iter: &mut Peekable<Chars>) -> (tok::Token, String){
-        let mut identifier_str: String = String::from("");
+    fn parse_unary(negate: bool, tok: Token, string_val: String) -> UnaryExprAST {
+        match tok {
+            Token::IDENTIFIER => {
+                UnaryExprAST { 
+                    negate: (negate), 
+                    child: (Box::new(VariableExprAST {
+                        name: string_val,
+                    })
+                )}
+            },
 
-        loop {
+            Token::NUMBER => {
+                UnaryExprAST { 
+                    negate: (negate), 
+                    child: (Box::new(VariableExprAST {
+                        name: string_val.parse().unwrap(),
+                    })
+                )}
+            },
 
-            if let Some(valid_char) = char_iter.next() {
+            _ => {
+                panic!("Unary expression must have variable or number as a child.")
+            }
+        }
+    }
 
-                if valid_char.is_alphabetic() { //IDENTIFIER
-                    identifier_str.push(valid_char);
+    fn gettok(chars: &mut Peekable<Chars>) -> (Token, String) { //Returns and consumes the current token
+        let mut tok_str = String::from("");
 
-                    while let Some(&next_char) = char_iter.peek() {
-                        if next_char.is_alphanumeric() {
-                            identifier_str.push(next_char);
-                            char_iter.next();               
-                        } else {
-                            break;
-                        }
+        while let Some(c) = chars.next() {
+
+            if c == ' ' {
+                loop {
+                    if chars.peek() == Some(&' ') {
+                        chars.next();
+                        continue;
                     }
 
-                    if identifier_str == "def" {
-                        return (tok::Token::Def, identifier_str);
-                    } else if identifier_str == "extern" {
-                        return (tok::Token::Extern, identifier_str);
+                    return (Token::WHITESPACE, tok_str);
+                }
+            }
+
+            else if c == '\t' || c == '\n' {
+                return (Token::MISC, tok_str);
+            }
+
+            else if c == '+' || c == '-' || c == '/' || c == '*' {
+                return (Token::OPERATOR, String::from(c));
+            }
+
+            else if c == '=' {
+                return (Token::EQUAL, String::from(c));
+            }
+
+            else if c == '(' {
+                return (Token::OPENPARENT, String::from(c));
+            }
+
+            else if c == ')' {
+                return (Token::CLOSEPARENT, String::from(c));
+            }
+
+            else if c.is_alphabetic() {
+                tok_str.push(c);
+
+                while let Some(&c_next) = chars.peek() {
+                    if c_next.is_alphanumeric() {
+                        tok_str.push(c_next);
+                        chars.next();
                     } else {
-                        return (tok::Token::Identifier, identifier_str);
+                        break;
                     }
                 }
 
-                else if valid_char.is_numeric() { //NUMBER
-                    identifier_str.push(valid_char);
+                return (Token::IDENTIFIER, tok_str);
+            }
 
-                    while let Some(&valid_char) = char_iter.peek() {
-                        if valid_char.is_numeric() {
-                            identifier_str.push(valid_char);
-                            char_iter.next();
-                        } else {
-                            break;
-                        }
+            else if c.is_numeric() {
+                tok_str.push(c);
+
+                while let Some(&c_next) = chars.peek() {
+                    if c_next.is_numeric() {
+                        tok_str.push(c_next);
+                        chars.next();
+                    } else {
+                        break;
                     }
-
-                    return (tok::Token::Number, identifier_str);
                 }
 
-                else if valid_char == '=' {
-                    return (tok::Token::Equal, String::from(valid_char));
-                }
-
-                else if valid_char == '+' || valid_char == '-' || valid_char == '*' || valid_char == '/' { //OPERATOR
-                    return (tok::Token::Operator, String::from(valid_char));
-                }
-
-                else if valid_char == '(' { //OPEN PARENTHESIS
-                    return (tok::Token::OpenParen, String::from("("));
-                }
-
-                else if valid_char == ')' { //CLOSE PARENTHESIS - Need this for error checking
-                    return (tok::Token::CloseParen, String::from(")"));
-                }
-
-                else if valid_char == ',' {
-                    return (tok::Token::Comma, String::from(","));
-                }
-
-                else { //WHITESPACES
-                    continue;
-                }
-
-            } else { //EOF
-                return (tok::Token::EOF, String::from(""));
+                return (Token::NUMBER, tok_str);
             }
-        }
-    }
 
-    fn lookahead_tok(char_iter: &mut Peekable<Chars>) -> tok::Token {
-
-        while let Some(&peek_char) = char_iter.peek() {
-            if peek_char == ' ' {
-                char_iter.next();
-            } else {
-                break;
-            }
         }
 
-        let lookahead_char = char_iter.peek();
-
-        if lookahead_char == None {
-            return tok::Token::EOF;
-        } else if lookahead_char == Some(&'+') || lookahead_char == Some(&'-') || lookahead_char == Some(&'*') || lookahead_char == Some(&'/') {
-            return tok::Token::Operator;
-        } else if lookahead_char == Some(&'=') {
-            return tok::Token::Equal;
-        } else if lookahead_char.unwrap().is_numeric() {
-            return tok::Token::Number;
-        } else if lookahead_char.unwrap().is_alphabetic() {
-            return tok::Token::Identifier;
-        } else {
-            return tok::Token::EOF;
-        }
+        return (Token::EOF, String::from(""));
     }
 
-    pub fn explore_tree(&self, file: &mut File) {
-        self.root.write_instruction(file);
+    fn scantok(chars: &mut Peekable<Chars>) -> (Token, String) { //Returns the next token without consuming
+        return Self::gettok(&mut chars.clone()); //TODO: Find a way to remove clone
     }
+
 }
