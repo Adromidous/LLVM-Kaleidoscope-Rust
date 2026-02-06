a387681eb1cd60652227478535efbd3a82bf131c
Adromidous <t24kumar17@gmail.com>
2026-02-06 17:55:44 -0500

    Starting recursive descent function

--- src/Parser/parser.rs
+++ src/Parser/parser.rs
@@ -13,6 +13,8 @@ pub enum ExprAST {
     IdentifierExprAST(String),
     UnaryExprAST{negate: bool, value: Box<ExprAST>},
     BinaryExprAST{operator: String, lhs: Box<ExprAST>, rhs: Box<ExprAST>},
+    ParentExprAST(Vec<ExprAST>),
+    Error,
     EOFExprAST{}
 }
 
@@ -32,16 +34,100 @@ impl Parser {
     }
 
     fn recursive_descent(chars: &mut Peekable<Chars>) -> ExprAST {
-        let (tok, val) = Self::gettok(chars);
 
+        loop {
+            let (tok, val) = Self::gettok(chars); 
+
+            if tok == Token::WHITESPACE || tok == Token::MISC {
+                continue;
+            } else if tok != Token::EOF {  
+
+                match tok {
+
+                    Token::IDENTIFIER => {
+                        
+                        let (tok_scan, val_scan) = Self::scantok(chars);
+
+                        match tok_scan => {
+
+                            Token::OPERATOR | Token::EQUAL => {
+                                Self::gettok(chars); //Consume token
+                                
+                                let lhs: ExprAST::IdentifierExprAST(val); 
+
+                                Self::parse_binary_expr(val_scan, lhs, Self::recursive_descent(chars));   
+                            }
+
+                            _ => {
+                                Self::parse_variable(val)
+                            }
+
+                        }
+                    },
+
+                    Token::NUMBER => {
+
+                        let (tok_scan, val_scan) = Self::scantok(chars);
+
+                        match tok_scan => {
+
+                            Token::OPERATOR | Token::EQUAL => {
+                                Self::gettok(chars); //Consume token
+                                
+                                let lhs: ExprAST::NumberExprAST(val.parse().unwrap()); 
+
+                                Self::parse_binary_expr(val_scan, lhs, Self::recursive_descent(chars));   
+                            }
+
+                            _ => {
+                                Self::parse_variable(val)
+                            }
+
+                        }
+
+                    },
+
+
+                    Token::OPERATOR | Token::EQUAL | Token::CLOSEPARENT => {
+                        ExprAST::Error
+                    },
+
+                    Token::OPENPARENT => {
+                       Vec<ExprAST> expressions = Vec::new(); 
+
+                        while let currExpr = Self::recursive_descent(chars) {
+                            
+                            match currExpr => {
+                               ExprAST::NumberExprAST | ExprAST::VariableExprAST | ExprAST::UnaryExprAST | ExprAST::BinaryExprAST => {
+                                   expressions.push(currExpr);
+                               }
+
+                                _ => {
+                                    ParentExprAST(expressions)
+                                }
+
+                            }
+
+                        }
+                         
+
+                    }
+                }
+
+            }
 
         ExprAST::EOFExprAST {  }
+        }
+        
     }
 
     fn parse_number(value: usize) -> ExprAST::NumberExprAST {
         ExprAST::NumberExprAST(value)
     }
 
+    fn parse_variable(identifier: String) -> ExprAST::IdentifierExprAST {
+        ExprAST::IdentifierExprAST(identifier)
+    }
 
     fn gettok(chars: &mut Peekable<Chars>) -> (Token, String) { //Returns and consumes the current token
         let mut tok_str = String::from("");
